90416b7f89dae3bfc3a9a1cabc0072d633f2d545
Kietne12 <hathekiet.td2004@gmail.com>
2026-03-16 21:22:11 +0700

    refactor: change user in authController to account

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -7,29 +7,37 @@ export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Không có token, truy cập bị từ chối" });
+    return res.status(401).json({
+      message: "Không có token, truy cập bị từ chối",
+    });
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // { id, email, role }
+
+    req.user = decoded; // { id, role }
+
     next();
   } catch (error) {
-    return res.status(403).json({ message: "Token không hợp lệ hoặc đã hết hạn" });
+    return res.status(403).json({
+      message: "Token không hợp lệ hoặc đã hết hạn",
+    });
   }
 };
 
 /**
  * Middleware kiểm tra role
- * @param  {...string} roles - Danh sách role được phép truy cập
  */
 export const authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Bạn không có quyền truy cập" });
+      return res.status(403).json({
+        message: "Bạn không có quyền truy cập",
+      });
     }
+
     next();
   };
 };
