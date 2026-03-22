d6cbcf82b5252ca78a19ddd935964a006812cbfe
Kietne12 <hathekiet.td2004@gmail.com>
2026-03-22 18:37:07 +0700

    push code

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -124,7 +124,7 @@ export const login = async (req, res) => {
   } catch (error) {
     console.error("Login error:", error);
     return res.status(500).json({
-      message: "Lỗi server",
+      message: "Đăng nhập không thành công do sai quá nhiều!",
     });
   }
 };
